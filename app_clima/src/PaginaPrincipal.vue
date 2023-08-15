f1f8ca432901438fd4d4b9b58251020c61dd2d67
sfreitascecilia <sfreitascecilia@gmail.com>
2023-08-15 17:49:07 -0300

    Atualização do projeto app_clima

---
+++ app_clima/src/PaginaPrincipal.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>App Clima</h1>
+  <div class="lista">
+    <div class="card" v-for="cidade in cidades" :key="cidade.id">
+      <div class="card-img"></div>
+      <h3>{{ cidade.name }}</h3>
+      <h4>{{ cidade.description }}</h4>
+      <div class="temperatura">{{ cidade.temp }}º</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import './assets/estilo.css'
+
+export default {
+    data() {
+      return {
+        cidades: [
+          {
+            id: 8429,
+            name: "Rio de Janeiro",
+            description: "Ensolarado",
+            icon: "01d",
+            temp: "25",
+          },
+          {
+            id: 8412,
+            name: "Manaus",
+            description: "Chuvas Fortes",
+            icon: "09d",
+            temp: "25",
+          },
+          {
+            id: 8454,
+            name: "Brasília",
+            description: "Ensolarado",
+            icon: "10d",
+            temp: "27.4",
+          },
+        ],
+      };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- app_clima/src/main.js
+++ app_clima/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import PaginaPrincipal from './PaginaPrincipal.vue'
 
-createApp(App).mount('#app')
+createApp(PaginaPrincipal).mount('#app')
